92b804a99d5eb969ff1e94b742e4d285226e136d
Luis Cruz <luiscruz.dev@gmail.com>
2018-09-29 04:45:00 +0200

    Add PointsActivity and remove unused icon

---
+++ app/src/main/java/com/thecomebacks/whatsthat/PointsActivity.java
@@ -0,0 +1,13 @@
+package com.thecomebacks.whatsthat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points);
+    }
+}
