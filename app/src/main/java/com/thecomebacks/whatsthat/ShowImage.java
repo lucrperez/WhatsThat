71b175957248570b2171350bc4f992a7dbf7ee35
Luis Cruz <luiscruz.dev@gmail.com>
2018-09-29 13:34:49 +0200

    Modify ShowImage Activity to use phone Camera

--- app/src/main/java/com/thecomebacks/whatsthat/ShowImage.java
+++ app/src/main/java/com/thecomebacks/whatsthat/ShowImage.java
@@ -1,6 +1,8 @@
 package com.thecomebacks.whatsthat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,13 +10,19 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,11 +36,14 @@ import com.thecomebacks.whatsthat.beans.User;
 import com.thecomebacks.whatsthat.commons.Constants;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ShowImage extends AppCompatActivity {
 
@@ -45,7 +56,11 @@ public class ShowImage extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_READ_CAMERA = 100;
     private static final int MY_PERMISSIONS_REQUEST_STORE = 200;
 
-    private boolean grantedPermissions = false;
+    private static final int TAKE_PICTURE = 10;
+    private Uri imageUri;
+
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int MEDIA_TYPE_VIDEO = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -155,34 +170,97 @@ public class ShowImage extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkCameraHardware(getApplicationContext()) && checkCameraPermission() && checkStorePermission()) {
-                    Intent intent = new Intent();
-                    intent.setClass(getApplicationContext(), CameraActivity.class);
-                    startActivity(intent);
+                if (checkCameraHardware(getApplicationContext())) {
+                    checkPermissions();
                 }
             }
         });
+
+        StrictMode.VmPolicy.Builder newbuilder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(newbuilder.build());
     }
 
-    private boolean checkCameraPermission() {
-        grantedPermissions = false;
+    private void checkPermissions() {
         int permissionCheck = ContextCompat.checkSelfPermission(ShowImage.this, Manifest.permission.CAMERA);
         if (permissionCheck == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(ShowImage.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_READ_CAMERA);
-            return grantedPermissions;
         } else {
-            return true;
+            permissionCheck = ContextCompat.checkSelfPermission(ShowImage.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(ShowImage.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_STORE);
+            } else {
+                /*Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), CameraActivity.class);
+                startActivity(intent);*/
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File photo = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photo));
+                imageUri = Uri.fromFile(photo);
+                startActivityForResult(intent, TAKE_PICTURE);
+            }
         }
     }
 
-    private boolean checkStorePermission() {
-        grantedPermissions = false;
-        int permissionCheck = ContextCompat.checkSelfPermission(ShowImage.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(ShowImage.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CAMERA);
-            return grantedPermissions;
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
         } else {
-            return true;
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    //getContentResolver().notifyChange(selectedImage, null);
+                    //ImageView imageView = (ImageView) findViewById(R.id.ImageView);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+
+                        //imageView.setImageBitmap(bitmap);
+                        /*Toast.makeText(this, selectedImage.toString(),
+                                Toast.LENGTH_LONG).show();*/
+                        // TODO convertir bitmap a Base64 y enviar al backend
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
         }
     }
 
@@ -193,7 +271,7 @@ public class ShowImage extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    grantedPermissions = true;
+                    checkPermissions();
                 }
                 return;
             }
@@ -201,7 +279,7 @@ public class ShowImage extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    grantedPermissions = true;
+                    checkPermissions();
                 }
                 return;
             }
